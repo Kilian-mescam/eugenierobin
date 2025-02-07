fd53e39ee4855cb7ba09c44173bf5a82b9ef40de
Kilian <kmescam@gmail.com>
2025-02-07 14:52:19 +0100

    change from image another

--- src/components/SkillCards.tsx
+++ src/components/SkillCards.tsx
@@ -1,12 +1,11 @@
 import { cn } from "@/lib/utils";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Card } from "./ui/card";
 import Image from 'next/image'
 import { useState } from "react";
 
 type Props = {
     image: string,
     imageBackground: string
-    imageModified?: string
     content: string,
     className?: string
     alt: string
@@ -35,25 +34,25 @@ export function SkillCards() {
             <div className=" flex flex-col items-start mx-14 gap-6 relative">
                 <div className="relative">
                     <Image 
-                        className={cn("transition-all duration-500 ease-in-out transform translate-y-2" , hoveredCard['1'] ? "-translate-y-16 -rotate-45" : "")}
-                        src="/images/star6.svg"
+                        className={cn("transition-all duration-700 ease-in-out transform" , hoveredCard['1'] ? "-translate-y-16 -rotate-45 " : "")}
+                        src={hoveredCard['1'] ? "/images/star6-modified.svg" : "/images/star6.svg"}
                         width={180}
                         height={180}
                         sizes="200px"
                         alt="ux"
                         priority={true}
                     />
-                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-22 left-16 transition-all duration-500 ease-in-out transform", hoveredCard['1'] ? "-translate-y-16" : "")}>UX</h1>
+                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-22 left-16 transition-all duration-700 ease-in-out transform", hoveredCard['1'] ? "-translate-y-16" : "")}>UX</h1>
                 </div>
                 <h3
                     className={cn(
-                        "absolute top-36 leading-8 font-light italic text-3xl text-white transition-all duration-500 ease-in-out transform", // Animation classes
+                        "absolute top-36 leading-8 font-light italic text-3xl text-white transition-all duration-700 ease-in-out transform", // Animation classes
                         hoveredCard['1'] ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-0" // Opacity and translation on hover
                     )}
                 >
                     Design d'expérience utilisateur
                 </h3>
-                <div className={cn('text-white text-lg transition-all duration-500 ease-in-out transform ', hoveredCard['1'] ? "translate-y-14" : "")}>
+                <div className={cn('text-white text-lg transition-all duration-700 ease-in-out transform ', hoveredCard['1'] ? "translate-y-14" : "")}>
                     J’audite et conçois des interfaces pour simplifier la vie des utilisateurs et répondre efficacement à leurs besoins
                 </div>
             </div>
@@ -68,25 +67,25 @@ export function SkillCards() {
             <div className=" flex flex-col items-start mx-14 gap-6 relative">
                 <div className="relative">
                     <Image 
-                        className={cn("transition-all duration-500 ease-in-out transform translate-y-2" , hoveredCard['2'] ? "-translate-y-16 rotate-90" : "")}
-                        src="/images/star4.svg"
+                        className={cn("transition-all duration-700 ease-in-out transform translate-y-2" , hoveredCard['2'] ? "-translate-y-16 rotate-90" : "")}
+                        src={hoveredCard['2'] ? "/images/star4-modified.svg" : "/images/star4.svg"}
                         width={180}
                         height={180}
                         sizes="200px"
                         alt='ui'
                         priority={true}
                     />
-                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-18 transition-all duration-500 ease-in-out transform", hoveredCard['2'] ? "-translate-y-16" : "")}>UI</h1>
+                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-18 transition-all duration-700 ease-in-out transform", hoveredCard['2'] ? "-translate-y-16" : "")}>UI</h1>
                 </div>
                 <h3
                     className={cn(
-                        "w-1/2 absolute top-38 leading-8 font-light italic text-3xl text-white transition-all duration-500 ease-in-out transform", // Animation classes
+                        "w-1/2 absolute top-38 leading-8 font-light italic text-3xl text-white transition-all duration-700 ease-in-out transform", // Animation classes
                         hoveredCard['2'] ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-0" // Opacity and translation on hover
                     )}
                 >
                     Design d'interface utilisateur
                 </h3>
-                <div className={cn('text-white text-lg transition-all duration-500 ease-in-out transform ', hoveredCard['2'] ? "translate-y-16" : "")}>J’harmonise les designs en respectant votre charte graphique, les spécificités de votre plateforme</div>
+                <div className={cn('text-white text-lg transition-all duration-700 ease-in-out transform ', hoveredCard['2'] ? "translate-y-16" : "")}>J’harmonise les designs en respectant votre charte graphique, les spécificités de votre plateforme</div>
             </div>
         </Card>
         <Card 
@@ -98,25 +97,25 @@ export function SkillCards() {
             <div className=" flex flex-col items-start mx-14 gap-6 relative">
                 <div className="relative">
                     <Image 
-                        className={cn("transition-all duration-500 ease-in-out transform translate-y-2" , hoveredCard['3'] ? "-translate-y-10 rotate-90" : "")}
-                        src="/images/star3.svg"
+                        className={cn("transition-all duration-700 ease-in-out transform translate-y-2" , hoveredCard['3'] ? "-translate-y-10 rotate-90" : "")}
+                        src={hoveredCard['3'] ? "/images/star3-modified.svg" : "/images/star3.svg"}
                         width={180}
                         height={180}
                         sizes="200px"
                         alt="branding"
                         priority={true}
                     />
-                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-7 transition-all duration-500 ease-in-out transform", hoveredCard['3'] ? "-translate-y-14" : "")}>Branding</h1>
+                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-7 transition-all duration-700 ease-in-out transform", hoveredCard['3'] ? "-translate-y-14" : "")}>Branding</h1>
                 </div>
                 <h3
                     className={cn(
-                        "w-1/2 absolute top-40 leading-8 font-light italic text-3xl text-white transition-all duration-500 ease-in-out transform", // Animation classes
+                        "absolute top-40 leading-8 font-light italic text-3xl text-white transition-all duration-700 ease-in-out transform", // Animation classes
                         hoveredCard['3'] ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-0" // Opacity and translation on hover
                     )}
                 >
                     Image de marque
                 </h3>
-                <div className={cn('text-white text-lg transition-all duration-500 ease-in-out transform ', hoveredCard['3'] ? "translate-y-8" : "")}>Je crée des logos et des identités visuelles qui reflètent votre univers et plaisent à votre audience</div>
+                <div className={cn('text-white text-lg transition-all duration-700 ease-in-out transform ', hoveredCard['3'] ? "translate-y-8" : "")}>Je crée des logos et des identités visuelles qui reflètent votre univers et plaisent à votre audience</div>
             </div>
         </Card>
         </div>
