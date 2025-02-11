8176debe4ba96842758ff2de1fb45676e54fd946
Kilian <kmescam@gmail.com>
2025-02-11 14:13:33 +0100

    anilmation on svg image

--- src/components/SkillCards.tsx
+++ src/components/SkillCards.tsx
@@ -32,17 +32,31 @@ export function SkillCards() {
             onMouseEnter={() => handleMouseEnter('1')}
             onMouseLeave={() => handleMouseLeave('1')}>
             <div className=" flex flex-col items-start mx-14 gap-6 relative">
-                <div className="relative">
-                    <Image 
-                        className={cn("transition-all duration-700 ease-in-out transform" , hoveredCard['1'] ? "-translate-y-16 -rotate-45 " : "")}
-                        src={hoveredCard['1'] ? "/images/star6-modified.svg" : "/images/star6.svg"}
-                        width={180}
-                        height={180}
-                        sizes="200px"
-                        alt="ux"
-                        priority={true}
-                    />
-                    <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-16 transition-all duration-700 ease-in-out transform", hoveredCard['1'] ? "-translate-y-16" : "")}>UX</h1>
+                <div className='relative'>
+                    <div className="absolute">
+                        <Image 
+                            className={cn("transition-all duration-700 ease-in-out transform" , hoveredCard['1'] ? "-translate-y-16 -rotate-45 opacity-0" : "opacity-100")}
+                            src="/images/star6.svg"
+                            width={180}
+                            height={180}
+                            sizes="200px"
+                            alt="ux"
+                            priority={true}
+                        />
+                        <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-16 transition-all duration-700 ease-in-out transform", hoveredCard['1'] ? "-translate-y-16" : "")}>UX</h1>
+                    </div>
+                    <div className="">
+                        <Image 
+                            className={cn("transition-all duration-700 ease-in-out transform" , hoveredCard['1'] ? "-translate-y-16 -rotate-45 opacity-10O" : "opacity-0")}
+                            src="/images/star6-modified.svg"
+                            width={180}
+                            height={180}
+                            sizes="200px"
+                            alt="ux"
+                            priority={true}
+                        />
+                        <h1 className={cn("italic text-white text-center text-3xl font-bold absolute top-20 left-16 transition-all duration-700 ease-in-out transform", hoveredCard['1'] ? "-translate-y-16" : "")}>UX</h1>
+                    </div>
                 </div>
                 <h3
                     className={cn(
